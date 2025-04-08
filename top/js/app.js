78b2bb53473052596f03f3bec665462363768840
mehran <taghavimehran99@gmail.com>
2025-04-08 22:07:17 +0330

    add and fix a lot of tag and styel in project and defind varebels in js file

---
+++ top/js/app.js
@@ -0,0 +1,30 @@
+// 4 input 
+
+//  من میخوام این 4 تا اینپوت مقدار 
+// هاشون وقتی که دکمه زده شده داحل خط 
+// بالاش نمایش  داده بشن
+
+
+
+
+// input haye man
+
+let one=document.getElementById('nameradder')
+let two=document.getElementById('task')
+let three=document.getElementById('moreinfo')
+let four=document.getElementById('deadtime')
+
+// show item
+
+let showList=document.getElementById('showList')
+// btn submit
+
+let btnSmit=document.getElementById('btn')
+
+
+btnSmit.addEventListener('click',(e)=>{
+    e.preventDefault();
+
+    showList.getElementsByTagName('li')
+
+})
